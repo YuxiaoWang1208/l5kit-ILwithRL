3939ceda0ddebf5cd5c3cf49bb043e790907e6ae
Xingyuan Dai <daixingyuan2015@ia.ac.cn>
2022-04-22 17:03:02 +0800

    update forward in OfflineRLModel

--- scripts/VectorizedOfflinRLModel.py
+++ scripts/VectorizedOfflinRLModel.py
@@ -3,6 +3,10 @@ from typing import List
 
 import torch
 from l5kit.planning.vectorized.open_loop_model import VectorizedModel
+from l5kit.planning.vectorized.common import build_target_normalization, pad_avail, pad_points
+# from .global_graph import MultiheadAttentionGlobalHead, VectorizedEmbedding
+# from .local_graph import LocalSubGraph, SinusoidalPositionalEmbedding
+
 from torch import nn
 
 
@@ -51,4 +55,71 @@ class VectorizedUnrollModel(VectorizedModel):
 
 
     def forward(self, data_batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        pass
+        # Load and prepare vectors for the model call, split into map and agents
+
+        # ==== LANES ====
+        # batch size x num lanes x num vectors x num features
+        polyline_keys = ["lanes_mid", "crosswalks"]
+        if not self.disable_lane_boundaries:
+            polyline_keys += ["lanes"]
+        avail_keys = [f"{k}_availabilities" for k in polyline_keys]
+
+        max_num_vectors = max([data_batch[key].shape[-2] for key in polyline_keys])
+
+        map_polys = torch.cat([pad_points(data_batch[key], max_num_vectors) for key in polyline_keys], dim=1)
+        map_polys[..., -1].fill_(0)
+        # batch size x num lanes x num vectors
+        map_availabilities = torch.cat([pad_avail(data_batch[key], max_num_vectors) for key in avail_keys], dim=1)
+
+        # ==== AGENTS ====
+        # batch_size x (1 + M) x seq len x self._vector_length
+        agents_polys = torch.cat(
+            [data_batch["agent_trajectory_polyline"].unsqueeze(1), data_batch["other_agents_polyline"]], dim=1
+        )
+        # batch_size x (1 + M) x num vectors x self._vector_length
+        agents_polys = pad_points(agents_polys, max_num_vectors)
+
+        # batch_size x (1 + M) x seq len
+        agents_availabilities = torch.cat(
+            [
+                data_batch["agent_polyline_availability"].unsqueeze(1),
+                data_batch["other_agents_polyline_availability"],
+            ],
+            dim=1,
+        )
+        # batch_size x (1 + M) x num vectors
+        agents_availabilities = pad_avail(agents_availabilities, max_num_vectors)
+
+        # batch_size x (1 + M) x num features
+        type_embedding = self.type_embedding(data_batch).transpose(0, 1)
+        lane_bdry_len = data_batch["lanes"].shape[1]
+
+        # call the model with these features
+        outputs, attns = self.model_call(
+            agents_polys, map_polys, agents_availabilities, map_availabilities, type_embedding, lane_bdry_len
+        )
+
+        # calculate loss or return predicted position for inference
+        if self.training:
+            if self.criterion is None:
+                raise NotImplementedError("Loss function is undefined.")
+
+            xy = data_batch["target_positions"]
+            yaw = data_batch["target_yaws"]
+            if self.normalize_targets:
+                xy /= self.xy_scale
+            targets = torch.cat((xy, yaw), dim=-1)
+            target_weights = data_batch["target_availabilities"].unsqueeze(-1) * self.weights_scaling
+            loss = torch.mean(self.criterion(outputs, targets) * target_weights)
+            train_dict = {"loss": loss}
+            return train_dict
+        else:
+            pred_positions, pred_yaws = outputs[..., :2], outputs[..., 2:3]
+            if self.normalize_targets:
+                pred_positions *= self.xy_scale
+
+            eval_dict = {"positions": pred_positions, "yaws": pred_yaws}
+            if attns is not None:
+                eval_dict["attention_weights"] = attns
+            return eval_dict
+
