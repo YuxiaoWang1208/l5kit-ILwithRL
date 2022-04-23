703bcc436a90e6ce2bba58291c1d8c1055c1e816
Xingyuan Dai <daixingyuan2015@ia.ac.cn>
2022-04-23 17:15:58 +0800

    refactor offline_rl

--- scripts/offline_rl.py
+++ scripts/offline_rl.py
@@ -1,162 +1,173 @@
 import os
+import sys
+from pathlib import Path
+
 # from pycharm
-from matplotlib import pyplot as plt
 import numpy as np
 import torch
-from torch import nn, optim
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-from tempfile import gettempdir
-
 from l5kit.configs import load_config_data
-from l5kit.data import ChunkedDataset, LocalDataManager
+from l5kit.data import ChunkedDataset
+from l5kit.data import LocalDataManager
 from l5kit.dataset import EgoDatasetVectorized
 from l5kit.planning.vectorized.closed_loop_model import VectorizedUnrollModel
 from l5kit.planning.vectorized.open_loop_model import VectorizedModel
 from l5kit.vectorization.vectorizer_builder import build_vectorizer
-
+from torch import nn
+from torch import optim
+from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
 
-import sys
-from pathlib import Path
 project_path = str(Path(__file__).parents[1])
 print("project path: ", project_path)
 sys.path.append(project_path)
 print(sys.path)
 
 from scripts.vectorized_offlin_rl_model import VectorOfflineRLModel
+from pathlib import Path
 
 # prepare data path and load cfg
+
 os.environ["L5KIT_DATA_FOLDER"] = "/mnt/share_disk/user/public/l5kit/prediction"
 
-dm = LocalDataManager(None)
-# get config
 
-# Home path
-from pathlib import Path
-print(Path.home())
+# print(Path.home())
 # Project path
 # project_path = Path("/mnt/share_disk/user/daixingyuan/l5kit/")
 
-dm = LocalDataManager(None)
-# get config
-cfg = load_config_data(str(Path(project_path, "examples/urban_driver/config.yaml")))
 
-# ===== INIT DATASET
-# cfg["train_data_loader"]["key"] = "train.zarr"
-train_zarr = ChunkedDataset(dm.require(cfg["train_data_loader"]["key"])).open()
+def load_dataset(cfg, traffic_signal_scene_id):
+    dm = LocalDataManager(None)
+    # ===== INIT DATASET
+    # cfg["train_data_loader"]["key"] = "train.zarr"
+    train_zarr = ChunkedDataset(dm.require(cfg["train_data_loader"]["key"])).open()
 
-vectorizer = build_vectorizer(cfg, dm)
-train_dataset = EgoDatasetVectorized(cfg, train_zarr, vectorizer)
+    vectorizer = build_vectorizer(cfg, dm)
+    train_dataset = EgoDatasetVectorized(cfg, train_zarr, vectorizer)
 
-# todo demo for single scene
-traffic_signal_scene_id = 13
-train_dataset = train_dataset.get_scene_dataset(traffic_signal_scene_id)
-print(train_dataset)
+    # todo demo for single scene
+    train_dataset = train_dataset.get_scene_dataset(traffic_signal_scene_id)
+    print(train_dataset)
+    return train_dataset
 
 
 URBAN_DRIVER = "Urban Driver"
 OPEN_LOOP_PLANNER = "Open Loop Planner"
 OFFLINE_RL_PLANNER = "Offline RL Planner"
 
+
 # model_name = URBAN_DRIVER
 # model_name = OPEN_LOOP_PLANNER
-model_name = OFFLINE_RL_PLANNER
-
-weights_scaling = [1.0, 1.0, 1.0]
-
-_num_predicted_frames = cfg["model_params"]["future_num_frames"]
-_num_predicted_params = len(weights_scaling)
 
 
-if model_name == URBAN_DRIVER:
-    model = VectorizedUnrollModel(
-        history_num_frames_ego=cfg["model_params"]["history_num_frames_ego"],
-        history_num_frames_agents=cfg["model_params"]["history_num_frames_agents"],
-        num_targets=_num_predicted_params * _num_predicted_frames,
-        weights_scaling=weights_scaling,
-        criterion=nn.L1Loss(reduction="none"),
-        global_head_dropout=cfg["model_params"]["global_head_dropout"],
-        disable_other_agents=cfg["model_params"]["disable_other_agents"],
-        disable_map=cfg["model_params"]["disable_map"],
-        disable_lane_boundaries=cfg["model_params"]["disable_lane_boundaries"],
-        detach_unroll=cfg["model_params"]["detach_unroll"],
-        warmup_num_frames=cfg["model_params"]["warmup_num_frames"],
-        discount_factor=cfg["model_params"]["discount_factor"],
+def load_model(model_name):
+    weights_scaling = [1.0, 1.0, 1.0]
+
+    _num_predicted_frames = cfg["model_params"]["future_num_frames"]
+    _num_predicted_params = len(weights_scaling)
+
+    if model_name == URBAN_DRIVER:
+        model = VectorizedUnrollModel(
+            history_num_frames_ego=cfg["model_params"]["history_num_frames_ego"],
+            history_num_frames_agents=cfg["model_params"]["history_num_frames_agents"],
+            num_targets=_num_predicted_params * _num_predicted_frames,
+            weights_scaling=weights_scaling,
+            criterion=nn.L1Loss(reduction="none"),
+            global_head_dropout=cfg["model_params"]["global_head_dropout"],
+            disable_other_agents=cfg["model_params"]["disable_other_agents"],
+            disable_map=cfg["model_params"]["disable_map"],
+            disable_lane_boundaries=cfg["model_params"]["disable_lane_boundaries"],
+            detach_unroll=cfg["model_params"]["detach_unroll"],
+            warmup_num_frames=cfg["model_params"]["warmup_num_frames"],
+            discount_factor=cfg["model_params"]["discount_factor"],
+        )
+
+    elif model_name == OPEN_LOOP_PLANNER:
+        model = VectorizedModel(
+            history_num_frames_ego=cfg["model_params"]["history_num_frames_ego"],
+            history_num_frames_agents=cfg["model_params"]["history_num_frames_agents"],
+            num_targets=_num_predicted_params * _num_predicted_frames,
+            weights_scaling=weights_scaling,
+            criterion=nn.L1Loss(reduction="none"),
+            global_head_dropout=cfg["model_params"]["global_head_dropout"],
+            disable_other_agents=cfg["model_params"]["disable_other_agents"],
+            disable_map=cfg["model_params"]["disable_map"],
+            disable_lane_boundaries=cfg["model_params"]["disable_lane_boundaries"],
+        )
+    elif model_name == OFFLINE_RL_PLANNER:
+        model = VectorOfflineRLModel(
+            history_num_frames_ego=cfg["model_params"]["history_num_frames_ego"],
+            history_num_frames_agents=cfg["model_params"]["history_num_frames_agents"],
+            num_targets=_num_predicted_params * _num_predicted_frames,
+            weights_scaling=weights_scaling,
+            criterion=nn.L1Loss(reduction="none"),
+            global_head_dropout=cfg["model_params"]["global_head_dropout"],
+            disable_other_agents=cfg["model_params"]["disable_other_agents"],
+            disable_map=cfg["model_params"]["disable_map"],
+            disable_lane_boundaries=cfg["model_params"]["disable_lane_boundaries"],
+            cfg=cfg
+        )
+    else:
+        raise ValueError(f"{model_name=} is invalid")
+
+    return model
+
+
+def init_logger(log_name):
+    # tensorboard for log
+    # log_id = "7-debug"
+    log_id = (
+        f"signal_scene_{log_name['traffic_signal_scene_id']}"
+        f"-il_weight_{log_name['imitate_loss_weight']}"
+        f"-pred_weight_{log_name['pred_loss_weight']}"
+        f"-1"
     )
+    log_dir = Path(project_path, "logs")
+    writer = SummaryWriter(log_dir=f"{log_dir}/{model_name}-{log_id}")
+    return writer
 
-elif model_name == OPEN_LOOP_PLANNER:
-    model = VectorizedModel(
-        history_num_frames_ego=cfg["model_params"]["history_num_frames_ego"],
-        history_num_frames_agents=cfg["model_params"]["history_num_frames_agents"],
-        num_targets=_num_predicted_params * _num_predicted_frames,
-        weights_scaling=weights_scaling,
-        criterion=nn.L1Loss(reduction="none"),
-        global_head_dropout=cfg["model_params"]["global_head_dropout"],
-        disable_other_agents=cfg["model_params"]["disable_other_agents"],
-        disable_map=cfg["model_params"]["disable_map"],
-        disable_lane_boundaries=cfg["model_params"]["disable_lane_boundaries"],
-    )
-elif model_name == OFFLINE_RL_PLANNER:
-    model = VectorOfflineRLModel(
-        history_num_frames_ego=cfg["model_params"]["history_num_frames_ego"],
-        history_num_frames_agents=cfg["model_params"]["history_num_frames_agents"],
-        num_targets=_num_predicted_params * _num_predicted_frames,
-        weights_scaling=weights_scaling,
-        criterion=nn.L1Loss(reduction="none"),
-        global_head_dropout=cfg["model_params"]["global_head_dropout"],
-        disable_other_agents=cfg["model_params"]["disable_other_agents"],
-        disable_map=cfg["model_params"]["disable_map"],
-        disable_lane_boundaries=cfg["model_params"]["disable_lane_boundaries"],
-        cfg=cfg
-    )
-else:
-    raise ValueError(f"{model_name=} is invalid")
 
-# tensorboard for log
-# log_id = "7-debug"
-log_id = f"traffic_signal_scene_{traffic_signal_scene_id}-1"
-log_dir = Path(project_path, "logs")
-writer = SummaryWriter(log_dir=f"{log_dir}/{model_name}-{log_id}")
+def train(model, train_dataset, cfg, writer):
+    # todo
+    cfg["train_params"]["max_num_steps"] = int(1e8)
 
-# prepare for training
-train_cfg = cfg["train_data_loader"]
-train_dataloader = DataLoader(train_dataset, shuffle=train_cfg["shuffle"], batch_size=train_cfg["batch_size"],
-                              num_workers=train_cfg["num_workers"])
-device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
-model = model.to(device)
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    train_cfg = cfg["train_data_loader"]
+    train_dataloader = DataLoader(train_dataset, shuffle=train_cfg["shuffle"], batch_size=train_cfg["batch_size"],
+                                  num_workers=train_cfg["num_workers"])
+    losses_train = []
+    # training loops
+    tr_it = iter(train_dataloader)
 
-# training loops
-tr_it = iter(train_dataloader)
-cfg["train_params"]["max_num_steps"] = int(1e8)
-progress_bar = tqdm(range(cfg["train_params"]["max_num_steps"]))
-losses_train = []
-model.train()
-torch.set_grad_enabled(True)
+    # prepare for training
+    device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    model.train()
+    torch.set_grad_enabled(True)
 
-for n_iter in progress_bar:
-    try:
-        data = next(tr_it)
-    except StopIteration:
-        tr_it = iter(train_dataloader)
-        data = next(tr_it)
-    # Forward pass
-    data = {k: v.to(device) for k, v in data.items()}
-    result = model(data)
-    loss = result["loss"]
+    progress_bar = tqdm(range(cfg["train_params"]["max_num_steps"]))
+    for n_iter in progress_bar:
+        try:
+            data = next(tr_it)
+        except StopIteration:
+            tr_it = iter(train_dataloader)
+            data = next(tr_it)
+        # Forward pass
+        data = {k: v.to(device) for k, v in data.items()}
+        result = model(data)
+        loss = result["loss"]
 
-    writer.add_scalar('Loss/train', loss.item(), n_iter)
-    writer.add_scalar('Loss/train_policy_loss', result["loss_imitate"].item(), n_iter)
-    writer.add_scalar('Loss/train_prediction_loss', result["loss_other_agent_pred"].item(), n_iter)
+        writer.add_scalar('Loss/train', loss.item(), n_iter)
+        writer.add_scalar('Loss/train_policy_loss', result["loss_imitate"].item(), n_iter)
+        writer.add_scalar('Loss/train_prediction_loss', result["loss_other_agent_pred"].item(), n_iter)
 
-    # Backward pass
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    losses_train.append(loss.item())
-    progress_bar.set_description(f"loss: {loss.item()} loss(avg): {np.mean(losses_train)}")
+        losses_train.append(loss.item())
+        progress_bar.set_description(f"loss: {loss.item()} loss(avg): {np.mean(losses_train)}")
 
 
 # save model
@@ -171,6 +182,7 @@ if __name__ == '__main__':
 
     import argparse
     import os
+
     os.environ["_TEST_TUNE_TRIAL_UUID"] = "_"  # 在log路径不包含uuid, 这样可以是文件夹完全按照创建时间排序
 
     parser = argparse.ArgumentParser()
@@ -182,3 +194,20 @@ if __name__ == '__main__':
     imitate_loss_weight = args.imitate_loss_weight
     pred_loss_weight = args.pred_loss_weight
 
+    cfg = load_config_data(str(Path(project_path, "examples/urban_driver/config.yaml")))
+    cfg.update(vars(args))
+    print(cfg)
+
+    traffic_signal_scene_id = 13
+    train_dataset = load_dataset(cfg, traffic_signal_scene_id)
+    model_name = OFFLINE_RL_PLANNER
+    model = load_model(model_name)
+
+    log_name = {
+        "traffic_signal_scene_id": traffic_signal_scene_id,
+        "imitate_loss_weight": imitate_loss_weight,
+        "pred_loss_weight": pred_loss_weight,
+    }
+    logger = init_logger(log_name)
+
+    train(model, train_dataset, cfg, logger)
