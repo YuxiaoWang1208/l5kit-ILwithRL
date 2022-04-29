6cad6f8a68731f05465a0f2de2462f85ad2f26f6
Xingyuan Dai <daixingyuan2015@ia.ac.cn>
2022-04-29 21:55:20 +0800

    add bash script to run in parallel for training

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,8 @@
-python scripts/offline_rl.py --imitate_loss_weight 0 --pred_loss_weight 1 --cuda_id 0  &
-python scripts/offline_rl.py --imitate_loss_weight 1 --pred_loss_weight 0 --cuda_id 0 &
-python scripts/offline_rl.py --imitate_loss_weight 10 --pred_loss_weight 1 --cuda_id 1 &
-python scripts/offline_rl.py --imitate_loss_weight 1 --pred_loss_weight 1 --cuda_id 1 &
-python scripts/offline_rl.py --imitate_loss_weight 1 --pred_loss_weight 0.5 --cuda_id 2 &
-python scripts/offline_rl.py --imitate_loss_weight 1 --pred_loss_weight 0.1 --cuda_id 2 &
-python scripts/offline_rl.py --imitate_loss_weight 1 --pred_loss_weight 0.01 --cuda_id 3
+FLAG=20220423
+python scripts/offline_rl.py --imitate_loss_weight 0 --pred_loss_weight 1 --cuda_id 0 --flag $FLAG  &
+python scripts/offline_rl.py --imitate_loss_weight 1 --pred_loss_weight 0 --cuda_id 0 --flag $FLAG &
+python scripts/offline_rl.py --imitate_loss_weight 10 --pred_loss_weight 1 --cuda_id 1 --flag $FLAG &
+python scripts/offline_rl.py --imitate_loss_weight 1 --pred_loss_weight 1 --cuda_id 1 --flag $FLAG &
+python scripts/offline_rl.py --imitate_loss_weight 1 --pred_loss_weight 0.5 --cuda_id 2 --flag $FLAG &
+python scripts/offline_rl.py --imitate_loss_weight 1 --pred_loss_weight 0.1 --cuda_id 2 --flag $FLAG &
+python scripts/offline_rl.py --imitate_loss_weight 1 --pred_loss_weight 0.01 --cuda_id 3 --flag $FLAG
